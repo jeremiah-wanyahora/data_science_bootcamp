8b6c50d7a03c4b6e114444db989d2683de164895
Jeremiah Muraguri <jerewanya@gmail.com>
2025-04-27 11:10:34 +0300

    Day 1: Added basic variables and scripting

---
+++ week1_basics/day1_variables.py
@@ -0,0 +1,19 @@
+# Day 1: Python Basics - variables and Printing
+
+# Create simple variables
+name = "Jeremiah Muraguri"
+age = 27
+is_data_scientist = True
+salary = 85000
+
+#Print the variables
+print("Name:", name)
+print("Age", age)
+print("Is a Data Scientist:", is_data_scientist)
+print("Salary:", salary)
+
+#Check types
+print("Type of name:", type(name))
+print("Type of age:", type(age))
+print("Type of is_data_scientist:", type(is_data_scientist))
+print("Type of salary:", type(salary))
