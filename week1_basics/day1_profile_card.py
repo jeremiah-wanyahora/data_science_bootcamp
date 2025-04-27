9dc15aab61a5ba7538980877662b1e18d4ff67ac
Jeremiah Muraguri <jerewanya@gmail.com>
2025-04-27 11:58:16 +0300

    Added profile card that is user interactive

---
+++ week1_basics/day1_profile_card.py
@@ -0,0 +1,18 @@
+# Day 1 Stretch goal : Profile card using user input
+
+# Ask for user details
+name = input("Enter your name: ")
+email = input("Enter your email: ")
+age = input("Enter your age: ")
+phone_number = input("Enter your phone number")
+job_title = input("Enter your job title: ")
+country = input("Enter your country: ")
+
+#Create a profile summary
+print("\n--- Profile Card ---")
+print(f"Name: {name}")
+print(f"Email: {email}")
+print(f"Age: {age}")
+print(f"Phone Number: {phone_number}")
+print(f"Job Title: {job_title}")
+print(f"Country: {country}")
